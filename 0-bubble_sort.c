2dfb8d616a4d5eee4501e8bbafdec972439def2a
Waridi318 <rosenjeri7151@gmail.com>
2023-04-26 00:46:22 +0300

    TASK 0.3

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,11 +13,12 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp, flag;
 
+	
+	if (size < 2 || array == NULL)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
-		if (size < 2)
-			break;
-
 		flag = 0;
 		for (j = 0; j < (size - 1 - i); j++)
 		{
